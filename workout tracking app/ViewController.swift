a9f72728237faed3a43323d2135d8241fb15613e
Senan Haque <senanhaq@gmail.com>
2021-05-07 00:16:06 -0400

    Initial Commit

---
+++ workout tracking app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workout tracking app
+//
+//  Created by Senan Haque on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
